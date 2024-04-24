c316b420d8a0dd54189caa32bb37481a7af06eba
An Vo <votanlean@gmail.com>
2024-04-24 23:47:57 +0700

    Add basic auth

--- main.go
+++ main.go
@@ -7,10 +7,24 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	})
+	mux.HandleFunc("/basic-auth", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Authorization") // tell the cache that the response is vary base on the request Authorization header
+		authorizationHeader := r.Header.Get("Authorization")
+		if authorizationHeader == "" {
+			json.NewEncoder(w).Encode(map[string]string{"user": "anonymous user"})
+			return
+		}
+		username, password, ok := r.BasicAuth()
+		if !ok {
+			json.NewEncoder(w).Encode(map[string]string{"error": "auth failed"})
+			return
+		}
+
+		json.NewEncoder(w).Encode(map[string]string{"username": username, "password": password})
 	})
 	srv := &http.Server{
 		Addr:    ":4000",
--- readme.md
+++ readme.md
@@ -1,5 +1,19 @@
 # Readme
 
+## Migration
+
+Install migration tool (MacOS)
+
+```bash
+brew install golang-migrate
+```
+
+Create migration file
+
+```bash
+migrate create -seq -ext=.sql -dir=./migrations create_users_table
+```
+
 ## Run server
 
 ```bash
@@ -11,4 +25,18 @@ go run ./
 ```bash
 curl -i http://localhost:4000/ping
 
+```
+
+## Test basic authentication
+
+Create some username password:
+
+```bash
+CREDENTIALS=$(echo -n "alice@example.com:pa55word" | base64)
+```
+
+Send the request
+
+```bash
+curl -i -H "Authorization: Basic $CREDENTIALS" http://localhost:4000/basic-auth
 ```
